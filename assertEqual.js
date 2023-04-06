0ee357c1bd60b749bf4bfce142a6fe97d2d66f71
Shuayb Sheikh Ali <shuayb.sheikhali@torontomu.ca>
2023-04-06 06:35:57 +0300

    assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,14 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function (actual, expected) {
-
+  if (actual === expected) {
+    console.log(`👏 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤨 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual("Hi", "Hi");
+assertEqual(1, 67);
